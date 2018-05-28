48ce87886e85b2d55e0be4d046ab9e1a1a68eeb3
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-28 17:51:48 +0900

    Add delete devise
    投稿記事を削除する機能を追加。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,11 @@ class TweetsController < ApplicationController
     Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
