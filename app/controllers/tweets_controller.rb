d0a70f428c6b0f47844e6ef2e7eac288d4ab3cee
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-28 18:14:29 +0900

    Add edit function
    投稿記事の編集機能を実装した。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,17 @@ class TweetsController < ApplicationController
     tweet.destroy if tweet.user_id == current_user.id
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
