4ef18570520218982228bcbb963b11bc7b22e5de
kamikura <yusuke@yusuke-no-MacBook-Air.local>
2018-05-31 13:40:48 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  'tweets#index'
   get   'tweets'      => 'tweets#index'
   get   'tweets/new'  => 'tweets#new'
   post  'tweets'      => 'tweets#create'
+  delete  'tweets/:id'  => 'tweets#destroy'
+  patch  'tweets/:id' => 'tweets#update'
+  get   'tweets/:id/edit'  => 'tweets#edit'
+  get   'users/:id'   => 'users#show'
+  get   'tweets/:id'  => 'tweets#show'
 end
