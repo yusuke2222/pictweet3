53e65bf39578fb7a0f2dcebae423c7b19a969a17
kamikura <{y.rakumika@gmail.com}>
2018-05-31 14:22:33 +0900

    Edit routes
    resourcesを使用して記述。

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'tweets#index'
-  get   'tweets'      => 'tweets#index'
-  get   'tweets/new'  => 'tweets#new'
-  post  'tweets'      => 'tweets#create'
-  delete  'tweets/:id'  => 'tweets#destroy'
-  patch  'tweets/:id' => 'tweets#update'
-  get   'tweets/:id/edit'  => 'tweets#edit'
-  get   'users/:id'   => 'users#show'
-  get   'tweets/:id'  => 'tweets#show'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
