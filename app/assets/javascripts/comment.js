efca230aa2873976508f035c6e90e509fe6f7611
kamikura <{y.rakumika@gmail.com}>
2018-06-08 15:20:35 +0900

    Add jQuery
    非同期通信でコメントを保存する様にした。

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+  })
+})
