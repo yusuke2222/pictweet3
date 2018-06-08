c4382979678a698616224c49c88842ad6f35199f
kamikura <{y.rakumika@gmail.com}>
2018-06-08 15:46:43 +0900

    Edit controller
    リクエストされたformatによって処理を分けるようにした。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(text: cmment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
-    redirect_to "/tweets/#{@comment.tweet.id}"
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
   end
 
   private
