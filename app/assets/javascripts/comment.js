8883320789ba4e89d1ba1b6c13b6864555b49207
kamikura <{y.rakumika@gmail.com}>
2018-06-08 16:08:46 +0900

    Edit jQuery
    JSONをdonメソッドで受け取り、 HTMLを作成する様実装。
    また、エラー時の処理も実装。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,14 @@
 $(function(){
+  function buildHTML(comment){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,5 +20,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html)
+      $('.textbox').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
